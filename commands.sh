cc731a27cdd79cd24d7209150db8d6e0041e8260
kailashahirwar <ahikailash1@gmail.com>
2017-03-29 12:07:01 +0530

    Docker commands

--- commands.sh
+++ commands.sh
@@ -12,7 +12,7 @@ sudo docker rmi your-docker-image
 # To create a docker image using the Dockerfile(Path of the Dockerfile)
 sudo docker build -t new-docker-image:tag docker-file-path
 
-# To run a docker image
+# To run a docker image as a container
 sudo docker run docker-image-name:tag
 
 
