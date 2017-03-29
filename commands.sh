57aabcb6e81cd083b9d4869f30f771e00817104e
kailashahirwar <ahikailash1@gmail.com>
2017-03-29 12:05:04 +0530

    Docker commands

---
+++ commands.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# To stop all containers
+sudo docker stop $(sudo docker ps -aq)
+
+# To remove all containers
+sudo docker rm $(sudo docker ps -aq)
+
+# To remove a docker image
+sudo docker rmi your-docker-image
+
+# To create a docker image using the Dockerfile(Path of the Dockerfile)
+sudo docker build -t new-docker-image:tag docker-file-path
+
+# To run a docker image
+sudo docker run docker-image-name:tag
+
+
